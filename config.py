19c6c5a93f3416ef8d62c3d8f027d3b295f63a0e
Andrey Yudov <yudovv@gmail.com>
2019-10-15 17:30:38 +0200

    added .env

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
-TOKEN = '988763643:AAHDIxPvnl-Gwsi-GeabDgBl6Q_LoDQnAj4'
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+print('your TOKEN:' + TOKEN)
+
+# TOKEN = '988763643:AAHDIxPvnl-Gwsi-GeabDgBl6Q_LoDQnAj4'
 APP_NAME = 'andreybillbot'
